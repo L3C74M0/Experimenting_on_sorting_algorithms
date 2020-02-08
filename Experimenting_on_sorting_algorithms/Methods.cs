f2706d80976de36bcfa8f4c9b6e5a1c0e10f12cf
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2020-02-07 22:29:38 -0500

    First commit

--- Experimenting_on_sorting_algorithms/Methods.cs
+++ Experimenting_on_sorting_algorithms/Methods.cs
@@ -13,12 +13,43 @@ namespace Experimenting_on_sorting_algorithms {
 
         }
 
-        public int[] MergeSort(int[] array) {
+        public void MergeSort(int[] array, int left, int mid, int right) {
+            int[] temp = new int[25];
+            int i, eol, num, pos;
+            eol = (mid - 1);
+            pos = left;
+            num = (right - left + 1);
 
+            while ((left <= eol) && (mid <= right)) {
+                if (array[left] <= array[mid])
+                    temp[pos++] = array[left++];
+                else
+                    temp[pos++] = array[mid++];
+            }
 
+            while (left <= eol)
+                temp[pos++] = array[left++];
+            while (mid <= right)
+                temp[pos++] = array[mid++];
+            for (i = 0; i < num; i++) {
+                array[right] = temp[right];
+                right--;
+            }
+        }
+
+        public int[] MergeSort(int[] array, int left, int right) {
+            int mid;
+            if (right > left) {
+                mid = (right + left) / 2;
+                MergeSort(array, left, mid);
+                MergeSort(array, (mid + 1), right);
+                MergeSort(array, left, (mid + 1), right);
+            }
 
             return array;
         }
+
+
 
         public int[] BinaryTreeSort(int[] array) {
 
--- Experimenting_on_sorting_algorithms/Program.cs
+++ Experimenting_on_sorting_algorithms/Program.cs
@@ -7,7 +7,28 @@ using System.Threading.Tasks;
 namespace Experimenting_on_sorting_algorithms
 {
     class Program {
+        protected static Methods methods;
+        
         static void Main(string[] args) {
+            methods = new Methods();
+
+            int[] array = new int[10];
+            array[0] = 10;
+            array[1] = 9;
+            array[2] = 8;
+            array[3] = 7;
+            array[4] = 6;
+            array[5] = 5;
+            array[6] = 4;
+            array[7] = 3;
+            array[8] = 2;
+            array[9] = 1;
+
+            Console.WriteLine(array);
+
+            methods.MergeSort(array, 0, array.Length - 1);
+
+            Console.WriteLine(array);
         }
     }
 }
