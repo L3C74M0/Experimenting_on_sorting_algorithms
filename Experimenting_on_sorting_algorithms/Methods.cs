d0ffb7e52aa61c838310cf958636f5b46a6a0fa1
L3C74M0 <juandavidlectamocaicedo@gmail.com>
2020-02-07 21:25:51 -0500

    First commit

---
+++ Experimenting_on_sorting_algorithms/Methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experimenting_on_sorting_algorithms {
+    class Methods {
+
+
+        public Methods(){
+        
+
+        }
+
+        public int[] MergeSort(int[] array) {
+
+
+
+            return array;
+        }
+
+        public int[] BinaryTreeSort(int[] array) {
+
+
+            return array;
+        }
+
+    }
+
+
+
+}
